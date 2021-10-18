653fc684a4f3b2ac5b7863e3bccd0ea90ba1b3b6
Ryan Roberts <ryan.roberts@nationwidechildrens.org>
2021-10-18 09:40:24 -0400

    updated plotting to group like plots, changed output to pdf, added automated cell type assignments

--- HuOsteoPrimary.R
+++ HuOsteoPrimary.R
@@ -10,20 +10,14 @@ library(ggsci)
 set.seed(888)
 setwd("~/../lab/Analysis/Ryan/RProjects/CellTypeAnnRefs/")
 
-# Define plot themes
-
-gglayer_theme <- list(
-  theme_minimal(),
-  element_text(family = "DejaVu Sans"),
-  plot.title = element_text(hjust = 0),
-  coord_fixed()
-)
+# Start the graphics device
+pdf("HuOsteoPrimary.pdf")
 
 # Start with GSE152048
 # Make a list of sample names
 s <- c("BC5", "BC6", "BC10", "BC11", "BC16",
        "BC17", "BC20", "BC21", "BC22")
-qc <- c(18000, 25000, 25000, 30000, 70000, 
+qc <- c(18000, 25000, 25000, 30000, 70000,
         40000, 70000, 50000, 50000)
 path <- c("Conventional", "Conventional", "Conventional",
           "Conventional", "Conventional", "Chondroblastic",
@@ -45,9 +39,11 @@ if(!dir.exists("PrimaryTumor/GSE152048")) {
   }
 }
 
-# Create a vector that contains normalized Seurat objects for all GSE152048 samples
+# Create a vector that contains normalized Seurat objects for all GSE152048
+#   samples
 raw <- c()
-for(i in 1:9) {
+p <- list()
+for(i in 1:length(s)) {
   x <- tenx_load_qc(str_c("PrimaryTumor/GSE152048/",
                           s[i], "/"))
   x <- subset(x, subset = nCount_RNA < qc[i] & percent.mt <13)
@@ -55,7 +51,7 @@ for(i in 1:9) {
   x$type <- type[i]
   x$path <- path[i]
   x$gse <- "GSE152048"
-  x <- x %>%
+  raw[[s[i]]] <- x %>%
     NormalizeData() %>%
     FindVariableFeatures() %>%
     ScaleData() %>%
@@ -63,25 +59,25 @@ for(i in 1:9) {
     FindNeighbors(dims = 1:20) %>%
     FindClusters(resolution = 0.3) %>%
     RunUMAP(dims = 1:20)
-  print(DimPlot(x, pt.size = 1, label = T) +
+  p[[i]] <- (DimPlot(raw[[s[i]]], pt.size = 1, label = T, reduction = "umap") +
           coord_fixed() +
           theme(legend.position = "none") +
           ggtitle(str_c(s[i], " basic clustering")))
-  raw <- c(raw, x)
 }
 rm(x)
+plot_grid(plotlist = p, ncol = 3)
 
 # Transition to download and process GSE162454
 # Make a list of sample names
 s <- c("OS_1", "OS_2", "OS_3", "OS_4", "OS_5", "OS_6")
 qc <- c(50000, 45000, 23000, 50000, 50000, 45000)
-pre <- c("GSM4952363_", "GSM4952364_", "GSM4952365_", "GSM5155198_", "GSM5155199_", "GSM5155200_")
+pre <- c("GSM4952363_", "GSM4952364_", "GSM4952365_", "GSM5155198_",
+  "GSM5155199_", "GSM5155200_")
 
 if(!dir.exists("PrimaryTumor/GSE162454")) {
   tar_dir <- "PrimaryTumor/GSE162454"
   dir.create(tar_dir)
   geo_pre <- "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE162nnn/GSE162454/suppl/GSE162454_RAW.tar"
-  # gse_path <- str_c(geo_pre, s[i], ".matrix.tar.gz")
   tar_file <- str_c(tar_dir, "/", "GSE162454.tar.gz ")
   options(timeout = 300)
   download.file(geo_pre, destfile = tar_file, method = "auto")
@@ -93,13 +89,18 @@ if(!dir.exists("PrimaryTumor/GSE162454")) {
     mfile <- str_c(pre[i], s[i], "_matrix.mtx.gz")
     samp_dir <- str_c(tar_dir, "/", s[i])
     dir.create(samp_dir)
-    file.rename(str_c(tar_dir, "/", bfile), str_c(samp_dir, "/", "barcodes.tsv.gz"))
-    file.rename(str_c(tar_dir, "/", ffile), str_c(samp_dir, "/", "features.tsv.gz"))
-    file.rename(str_c(tar_dir, "/", mfile), str_c(samp_dir, "/", "matrix.mtx.gz"))
+    file.rename(str_c(tar_dir, "/", bfile),
+      str_c(samp_dir, "/", "barcodes.tsv.gz"))
+    file.rename(str_c(tar_dir, "/", ffile),
+      str_c(samp_dir, "/", "features.tsv.gz"))
+    file.rename(str_c(tar_dir, "/", mfile),
+      str_c(samp_dir, "/", "matrix.mtx.gz"))
   }
 }
 
-# Create a vector that contains normalized Seurat objects for all GSE162454 samples
+# Create a vector that contains normalized Seurat objects for all GSE162454
+#   samples
+p <- list()
 for(i in 1:length(s)) {
   x <- tenx_load_qc(str_c("PrimaryTumor/GSE162454/",
                           s[i], "/"))
@@ -108,7 +109,7 @@ for(i in 1:length(s)) {
   x$type <- "Primary"
   x$path <- "Conventional"
   x$gse <- "GSE162454"
-  x <- x %>%
+  raw[[s[i]]] <- x %>%
     NormalizeData() %>%
     FindVariableFeatures() %>%
     ScaleData() %>%
@@ -116,28 +117,25 @@ for(i in 1:length(s)) {
     FindNeighbors(dims = 1:20) %>%
     FindClusters(resolution = 0.3) %>%
     RunUMAP(dims = 1:20)
-  print(DimPlot(x, pt.size = 1, label = T) +
+  p[[i]] <- (DimPlot(raw[[s[i]]], pt.size = 1, label = T, reduction = "umap") +
           coord_fixed() +
           theme(legend.position = "none") +
           ggtitle(str_c(s[i], " basic clustering")))
-  raw <- c(raw, x)
 }
 rm(x)
+plot_grid(plotlist = p, ncol = 3)
 
-# Subsample and merge into one Seurat object and integrate with harmony (primary only)
+# Subsample and merge into one Seurat object and integrate with
+# harmony (primary only)
 prim <- c()
+raw <- raw[!names(raw) %in% c("BC10", "BC17")]
 for(i in 1:length(raw)) {
-  x <- subset(raw[[i]], cells = sample(Cells(raw[[i]]), 1000))
-  prim <- c(prim, x)
-  rm(x)
+  prim[[i]] <- subset(raw[[i]], cells = sample(Cells(raw[[i]]), 1000))
 }
 
 comb <- merge(prim[[1]], y = prim[2:length(prim)],
-                add.cell.ids = 1:length(prim),
+                add.cell.ids = names(raw),
                 project = "PrimaryReference")
-
-# Subset data to keep only primary tumors
-comb <- subset(comb, type=="Primary")
 
 comb <- comb %>%
   NormalizeData() %>%
@@ -146,8 +144,8 @@ comb <- comb %>%
   RunPCA(verbose = F)
 
 comb <- RunHarmony(comb, group.by.vars = "src")
-comb <- RunUMAP(comb, reduction = "harmony", dims = 1:30)
 comb <- comb %>%
+  RunUMAP(reduction = "harmony", dims = 1:30) %>%
   FindNeighbors(reduction = "harmony", dims = 1:30) %>%
   FindClusters()
 
@@ -156,13 +154,10 @@ qs::qsave(comb, "PrimaryTumor/comb.qs")
 # Start from this stopping point
 comb <- qs::qread("PrimaryTumor/comb.qs")
 
-theme_set(theme_sf_light())
-
 DimPlot(comb, reduction = "umap", label = T, repel = T) +
   coord_fixed() +
   ggtitle("Primary Tumors (integrated)") +
-  theme(legend.position = "none") +
-  theme_sf_light()
+  theme(legend.position = "none")
 
 DimPlot(comb, reduction = "umap", group.by = "src") +
   coord_fixed() +
@@ -173,88 +168,80 @@ DimPlot(comb, reduction = "umap", split.by = "src", ncol = 4) +
   ggtitle("Primary Tumors (integrated)")
 
 ## Optimizing UMAP parameters
+p <- list()
 for(i in 1:5) {
   test <- RunUMAP(comb, reduction = "harmony", dims = 1:30,
     set.op.mix.ratio = (i-1)*0.25)
   test <- test %>%
     FindNeighbors(reduction = "harmony", dims = 1:30) %>%
     FindClusters()
-  p <- DimPlot(test, reduction = "umap", label = T, repel = T) +
-    coord_fixed() +
-    ggtitle(str_c("set.op.mix.ratio = ", (i-1)*0.25)) +
-    theme(legend.position = "none")
-  print(p)
+  p[[i]] <- DimPlot(test, reduction = "umap", label = T, repel = T) +
+      coord_fixed() +
+      ggtitle(str_c("set.op.mix.ratio = ", (i-1)*0.25)) +
+      theme(legend.position = "none")
 }
+plot_grid(plotlist = p, ncol = 3)
 
+p <- list()
 for(i in 1:5) {
   test <- RunUMAP(comb, reduction = "harmony", dims = 1:30, 
     n.neighbors = (i * 10))
   test <- test %>%
     FindNeighbors(reduction = "harmony", dims = 1:30) %>%
     FindClusters()
-  p <- DimPlot(test, reduction = "umap", label = T, repel = T) +
-    coord_fixed() +
-    ggtitle(str_c("n.neighbors = ", (i * 10))) +
-    theme(legend.position = "none")
-  print(p)
+  p[[i]] <- DimPlot(test, reduction = "umap", label = T, repel = T) +
+      coord_fixed() +
+      ggtitle(str_c("n.neighbors = ", (i * 10))) +
+      theme(legend.position = "none")
 }
+plot_grid(plotlist = p, ncol = 3)
 
+p <- list()
 for(i in 1:5) {
   test <- RunUMAP(comb, reduction = "harmony", dims = 1:30, 
     n.epochs = (i * 100))
   test <- test %>%
     FindNeighbors(reduction = "harmony", dims = 1:30) %>%
     FindClusters()
-  p <- DimPlot(test, reduction = "umap", label = T, repel = T) +
+  p[[i]] <- DimPlot(test, reduction = "umap", label = T, repel = T) +
     coord_fixed() +
     ggtitle(str_c("n.epochs = ", (i * 100))) +
     theme(legend.position = "none")
-  print(p)
 }
+plot_grid(plotlist = p, ncol = 3)
 
+p <- list()
 for(i in 1:5) {
   test <- RunUMAP(comb, reduction = "harmony", dims = 1:30, 
     min.dist = (.00045 * 4 ^ i))
   test <- test %>%
     FindNeighbors(reduction = "harmony", dims = 1:30) %>%
     FindClusters()
-  p <- DimPlot(test, reduction = "umap", label = T, repel = T) +
+  p[[i]] <- DimPlot(test, reduction = "umap", label = T, repel = T) +
     coord_fixed() +
     ggtitle(str_c("min.dist = ", (.00045 * 4 ^ i))) +
     theme(legend.position = "none")
-  print(p)
 }
+plot_grid(plotlist = p, ncol = 3)
 
+p <- list()
 for(i in 1:5) {
   test <- RunUMAP(comb, reduction = "harmony", dims = 1:30, 
     local.connectivity = (2 ^ i))
   test <- test %>%
     FindNeighbors(reduction = "harmony", dims = 1:30) %>%
     FindClusters()
-  p <- DimPlot(test, reduction = "umap", label = T, repel = T) +
+  p[[i]] <- DimPlot(test, reduction = "umap", label = T, repel = T) +
     coord_fixed() +
     ggtitle(str_c("local.connectivity = ", (2 ^ i))) +
     theme(legend.position = "none")
-  print(p)
 }
+plot_grid(plotlist = p, ncol = 3)
 
-# # Ensure that groups are not just dominated by cell cycle effects
-# comb <- kill_cc(comb)
+# Plot out cell cycle
+comb <- kill_cc(comb)
 
-# # Set a post-cc-regression stopping point
-# qs::qsave(comb, "comb-ccReg.qs")
-# # Start from this stopping point
-# comb <- qs::qread("comb-ccReg.qs")
-
-# DimPlot(primary, reduction = "umap", label = T, repel = T) +
-#   coord_fixed() +
-#   ggtitle("GSE152048 composite - primaries") +
-#   theme(legend.position = "none")
-
-# DimPlot(primary, reduction = "umap", group.by = "src") +
-#   coord_fixed() +
-#   ggtitle("GSE152048 composite - primaries") 
-
+# Begin process of identifying cell types and annotating the samples
 # Calculate and show module scores from the paper
 ms <- list()
 ms$Osteoblastic <- c("RUNX2", "COL1A1", "CDH11", "IBSP")
@@ -279,10 +266,58 @@ for(i in 1:length(mod_names)) {
   comb <- AddModuleScore(comb, ms[i], name = names(ms[i]))
   g[[i]] <- FeaturePlot(comb, features = str_c(names(ms[i]), "1"), pt.size = 1,
     order = T, cols = c("lightgoldenrod", "darkred")) +
-    coord_fixed() +
-    theme(legend.position = "none")
+    coord_fixed()
 }
 plot_grid(plotlist = g, labels = LETTERS[1:length(g)], ncol = 4, nrow = 4)
 
-
 # Perform automated cell type assignment
+# Assign cell types (roughly, Human Primary Cell Atlas)
+ref <- celldex::HumanPrimaryCellAtlasData()
+comb_sce <- DietSeurat(comb)
+comb_sce <- as.SingleCellExperiment(comb_sce)
+
+# Make cell type predictions using SingleR
+comb_pred <- SingleR::SingleR(
+  test = comb_sce,
+  ref = ref,
+  labels = ref$label.main)
+
+# Transfer cell labels back to the Seurat object and plot
+comb$pred1 <- comb_pred$labels
+Idents(comb) <- comb$pred1
+DimPlot(comb, pt.size = 1, label = T, repel = T) +
+  coord_fixed() +
+  theme(legend.position = "none") +
+  ggtitle("Cell Type Predictions (Human Primary Cell Atlas)")
+DimPlot(comb, pt.size = 1, label = T, repel = T, split.by = "src", ncol = 4) +
+  coord_fixed() +
+  theme(legend.position = "none") +
+  ggtitle("Cell Type Predictions (Human Primary Cell Atlas) by sample")
+
+# Assign cell types (roughly, Blueprint/ENCODE)
+ref <- celldex::BlueprintEncodeData()
+
+# Make cell type predictions using SingleR
+comb_pred <- SingleR::SingleR(
+  test = comb_sce,
+  ref = ref,
+  labels = ref$label.main)
+
+# Transfer cell labels back to the Seurat object and plot
+comb$pred2 <- comb_pred$labels
+Idents(comb) <- comb$pred2
+DimPlot(comb, pt.size = 1, label = T, repel = T) +
+  coord_fixed() +
+  theme(legend.position = "none") +
+  ggtitle("Cell Type Predictions (Blueprint/ENCODE)")
+DimPlot(comb, pt.size = 1, label = T, repel = T, split.by = "src", ncol = 4) +
+  coord_fixed() +
+  theme(legend.position = "none") +
+  ggtitle("Cell Type Predictions (Blueprint/ENCODE) by sample")
+
+dev.off()
+
+# Save a stopping point - cell type predictions made
+qs::qsave(comb, "PrimaryTumor/comb-pred.qs")
+# Start from this stopping point
+comb <- qs::qread("PrimaryTumor/comb-pred.qs")
