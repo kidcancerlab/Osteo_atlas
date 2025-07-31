53a4f91975eac1da4851d05fa1a4faa6ed36bb59
matt-gust <matt.g0017@gmail.com>
2025-07-31 12:21:23 -0400

    made scripts for submitting rctd at different granularities as bash array

---
+++ rctd_array.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --account=gdrobertslab
+#SBATCH --output=output/spacexr/%j.txt
+#SBATCH --error=output/spacexr/%j.txt
+#SBATCH --job-name rctd
+#SBATCH --array=0-40
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=80G
+#SBATCH --partition=himem,general
+#SBATCH --time=24:00:00
+#SBATCH --wait
+
+# Get arrays holding object name and reference name
+while read object_name ref_name
+do
+    obj_array+=("$object_name")
+    ref_array+=("$ref_name")
+done < misc/rctd_input.txt
+
+ob_id=${obj_array[$SLURM_ARRAY_TASK_ID]}
+ref=${ref_array[$SLURM_ARRAY_TASK_ID]}
+
+echo Annotating $ob_id with $ref
+
+# move and rename output to somewhere findable
+mv output/spacexr/${SLURM_JOBID}.txt output/spacexr/granular_references/$ref/${ob_id}_output.txt
+
+Rscript rctd.r $ob_id $ref
